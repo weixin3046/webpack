dcd939497e9b9fd6ae754fab5c0c00695c4e4fdf
weixin4036 <408516111@qq.com>
2019-01-03 16:53:10 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 
 # 安装
-
  node -v
  npm -v 
 
@@ -9,15 +8,30 @@
 
 
 # 开始
-
  npm init
  npm install --save-dev webpack 局部安装在测试变量中
 
 
 # 命令
-
  webpack ./src/app.js ./dist/app.bundle.js 把 ./src/app.js 作为源文件，把转化后的结果放到 ./dist/app.bundle.js 文件中
+ 监听文件多加一个参数(watch)
+ webpack --watch ./src/app.js ./dist/app.bundle.js 
 
  webpack 打包 默认执行webpack.config.js
 
-#
+ webpack-dev-server 启动服务器
+
+# 插件
+ npm install --save-dev css-loader style-loader
+ npm i clean-webpack-plugin --save-dev 清除文件
+
+# react
+  npm install --save react react-dom
+
+  babel能把 react 代码转成一般浏览器可读可执行的代码，通常可以用它来转化 react 或 vue 这样的前端代码，或者把 es6 代码转成普通的 javascript 代码
+
+  npm install --save-dev babel-core babel-preset-react babel-preset-env babel-preset-es2015 安装babel
+  React preset https://babeljs.io/docs/plugins/preset-react/
+  React Installation https://reactjs.org/docs/installation.html
+
+  npm install --save-dev babel-loader 在 webpack 中使用一个 loader 来转化 react 的代码
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,90 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //清除文件
+const webpack = require('webpack');
+
+let pathsToClean = [
+	'dist',
+]
 
 module.exports = {
 	/**
+	 * entry 表示源文件，output 这边表示的是输出的目标文件
 	 * filename 名称
 	 * path 	路径
 	 */
 	entry: { //入口
-		main: './src/index.js'
+		'main': './src/app.js',
+		'contact': './src/contact.js'
+	},
+	devServer: { //服务器
+		port: 9000, //端口
+		open: true, //自动打开浏览器
+		hot: true
 	},
 	output: { //出口
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: '[name].[hash].js'
+	},
+	//插件都在plugins对象里配置
+	plugins: [
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new CleanWebpackPlugin(pathsToClean),
+		new HtmlWebpackPlugin({
+			template: './src/index.pug',
+			filename: 'app.html', //更改生成文件的名字
+			//可以把生成的 index.html 文件的内容的没用空格去掉，减少空间
+			minify: {
+				collapseWhitespace: true,
+			},
+			//hash: true 为了更好的 cache，可以在文件名后加个 hash
+			hash: true,
+			excludeChunks: ['contact']
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/contact.html',
+			filename: 'contact.html', //更改生成文件的名字
+			//可以把生成的 index.html 文件的内容的没用空格去掉，减少空间
+			minify: {
+				collapseWhitespace: true,
+			},
+			//hash: true 为了更好的 cache，可以在文件名后加个 hash
+			hash: true,
+			chunks: ['contact']
+		}),
+		// excludeChunks 指的是不包含， chunks 代表的是包含。
+		//new ExtractTextPlugin('style.css'),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+		})
+	],
+	module: {
+		rules: [{
+				test: /\.scss$/, //test: /\.css$/
+				// use: ['style-loader', 'css-loader', 'sass-loader']
+				// 把 SASS 或 CSS 处理好后，放到一个 CSS 文件中
+				use: [
+					MiniCssExtractPlugin.loader, //https://www.npmjs.com/package/mini-css-extract-plugin
+					"css-loader",
+					"sass-loader"
+				]
+				//这是extract-text-webpack-plugin插件的使用方法目前webpack4不支持
+				// use: ExtractTextPlugin.extract({
+				// 	fallback: 'style-loader',// 编译后用什么loader来提取css文件
+				// 	//resolve-url-loader may be chained before sass-loader if necessary
+				// 	use: ['css-loader', 'sass-loader']
+				// })
+			}, {
+				test: /\.pug$/,
+				loader: ['raw-loader', 'pug-html-loader']
+			}
+			// 这两行是处理 react 相关的内容
+			// { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+			// { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+		]
 	}
 };
